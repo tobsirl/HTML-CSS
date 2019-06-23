e47c8366f1fc8a39bc4f1ee70e248fe5e3f491e9
Paul Tobin <tobster111@gmail.com>
2019-06-23 19:28:49 +0100

    Add show to the tab content that has been clicked on

--- NetflixLandingPage/js/index.js
+++ NetflixLandingPage/js/index.js
@@ -1,14 +1,24 @@
 const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabContentItem = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
   removeBorder();
+  removeShow();
   this.classList.add('tab-border');
+
+  // Grad the content from the DOM
+  const tabContentitem = document.querySelector(`#${this.id}-content`);
+  // add show to the tab-1-content
+  tabContentitem.classList.add('show');
 }
 
 function removeBorder() {
   tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+  tabContentItem.forEach(item => item.classList.remove('show'));
 }
 // Listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
