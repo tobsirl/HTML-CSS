85218f5abb9cb3f1578231366b61b9966c1536f5
Paul Tobin <tobster111@gmail.com>
2019-06-23 19:20:54 +0100

    Added both hide and show in the css

--- NetflixLandingPage/js/index.js
+++ NetflixLandingPage/js/index.js
@@ -3,8 +3,12 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
+  removeBorder();
   this.classList.add('tab-border');
 }
 
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
 // Listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
