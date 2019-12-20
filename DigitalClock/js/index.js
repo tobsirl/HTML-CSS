84afe2b43be437f4d8e5fcdf6c70c8c291082447
Paul Tobin <tobster111@gmail.com>
2019-12-20 15:15:12 +0000

    Added the javascript for getting the time

--- DigitalClock/js/index.js
+++ DigitalClock/js/index.js
@@ -1,3 +1,15 @@
 function clock() {
-  const time = new Date()
+  const hours = document.getElementById('hour');
+  const minutes = document.getElementById('minute');
+  const seconds = document.getElementById('second');
+
+  const h = new Date().getHours();
+  const m = new Date().getMinutes();
+  const s = new Date().getSeconds();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+
 }
+const interval = setInterval(clock, 1000);
