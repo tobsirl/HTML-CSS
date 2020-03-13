53de85889b46d9fe9c08fb74782b1c1a04ea0dfe
Paul Tobin <tobster111@gmail.com>
2020-03-13 15:42:53 +0000

    Media query

--- ResponsivePortfolioPage/js/index.js
+++ ResponsivePortfolioPage/js/index.js
@@ -1,8 +1,6 @@
 function toggle() {
   const sec = document.getElementById('sec');
   const nav = document.getElementById('navigation');
-  sec.classList.toggle('active')
-  nav.classList.toggle('active')
-
-  
+  sec.classList.toggle('active');
+  nav.classList.toggle('active');
 }
