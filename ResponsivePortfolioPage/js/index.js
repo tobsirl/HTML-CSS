1f1b2e7dcef759f95984e69e030520d1223e087e
Paul Tobin <tobster111@gmail.com>
2020-03-13 15:05:15 +0000

    Styling of the header and the Know More button

--- ResponsivePortfolioPage/js/index.js
+++ ResponsivePortfolioPage/js/index.js
@@ -1,6 +1,8 @@
 function toggle() {
   const sec = document.getElementById('sec');
+  const nav = document.getElementById('navigation');
   sec.classList.toggle('active')
+  nav.classList.toggle('active')
 
-  sec.addEventListener('toggle')
+  
 }
