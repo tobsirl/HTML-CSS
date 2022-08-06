308188df12703733d92f1995feade79907850d35
Paul Tobin <tobster111@gmail.com>
2022-08-06 10:26:06 +0100

    toggleAttribute

--- Frontend Mentor/manage-landing-page/main.js
+++ Frontend Mentor/manage-landing-page/main.js
@@ -2,5 +2,5 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click', () => {
-  primaryNav.classList.toggle('is-open');
+  primaryNav.toggleAttribute('data-visible');
 });
