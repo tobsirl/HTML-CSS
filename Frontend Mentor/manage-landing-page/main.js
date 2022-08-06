619266d55972bd87306b9658db68f55d28b308b6
Paul Tobin <tobster111@gmail.com>
2022-08-06 10:30:37 +0100

    js

--- Frontend Mentor/manage-landing-page/main.js
+++ Frontend Mentor/manage-landing-page/main.js
@@ -2,5 +2,8 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.removeAttribute('aria-expanded', true);
   primaryNav.toggleAttribute('data-visible');
 });
